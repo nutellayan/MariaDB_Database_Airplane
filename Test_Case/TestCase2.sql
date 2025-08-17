5717b5b02556a939eccf6e7e32f4be672ed7db6d
Natalie_Uni <40677197@live.napier.ac.uk>
2025-08-17 20:47:59 +0100

    Delete Test_Case directory

--- Test_Case/TestCase2.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    t.ssn AS technician_ssn,
-    eu.union_id,
-    eu.union_membership_number,
-    a.model_number
-FROM technician t
-JOIN employee_union eu ON t.ssn = eu.ssn
-JOIN testing_event te ON t.ssn = te.technician_ssn
-JOIN airplane a ON te.registration_number = a.registration_number;
